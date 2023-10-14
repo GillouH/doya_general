9e32fab23ad86b2bca18799de61bd1f6bf2674f2
Gillou <gilles.hubert97@gmail.com>
2023-10-15 00:50:51 +0200

    [Back] - [Improve] - New header/footer cut suggestion

--- _footer.php
+++ _footer.php
@@ -1,9 +1,3 @@
-
-
-</body>
 <div id="footer">
     Lorem ipsum dolor sit amet consecte Footer <br> &copy; 2023
 </div>
-
-
-</html>
--- index.php
+++ index.php
@@ -24,11 +24,14 @@ if ($is_form_submitted) {
 }
 ?>
 
-<?php
-    include("_header.php");
-?>
-
-<!-- navbar  -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("_header.php"); ?>
+    <title>Doya_General</title>
+</head>
+<body>
+    <!-- navbar  -->
     <nav>
         <div class="logo"><a href="#"><img src="assets/images/logo.svg" alt="Not found"></a></div>
         <div class="soc">
@@ -39,51 +42,51 @@ if ($is_form_submitted) {
             </ul>
         </div>
     </nav>
-<!-- content  -->
+    <!-- content  -->
 
-<div id="welcome">
-<h1>Join<br>The Creative<br>Community</h1>
-    <!-- <br> -->
-        <p>To Access this website You Must Have an Account, <a href="#">Sign Up</a>
+    <div id="welcome">
+    <h1>Join<br>The Creative<br>Community</h1>
+        <!-- <br> -->
+            <p>To Access this website You Must Have an Account, <a href="#">Sign Up</a>
 
-        </p>
-    </div>
-
-
-    <button id="loginButton"> Login </button>
-
-
-    <div id="loginModal" class="modal">
-        <div class="modal-content">
-            <span class="close" id="closeModal"> <i class="bi-x"></i></span>
-            <h2 style="text-align: center;margin-top: -20px;" >Login</h2><br>
-            <form method="post" id="login">
-                    <legend style="margin-top: -20px;">Guess a Username and Password !</legend>
-                    <p>
-                        <label for="user"><i class="bi-person"></i></label>
-                        <input type="text" placeholder="Username" value="<?php echo $pseudo ? $pseudo : ""; ?>" name="pseudo" id="user" size="20" maxlength="10" required autofocus />
-                    </p>
-                    <p>
-                        <label for="psw"><i class="bi-key"></i> </label>
-                        <input type="password" placeholder="*******" value="<?php echo $pseudo ? $password : ""; ?>" name="password" id="psw" size="20" required />
-                    </p>
-                    <input type="submit" value="Test credential !" id="button" style="margin-top: 30px;">
-
-            </form>
+            </p>
         </div>
-    </div>
 
 
-<!-- Blob  -->
-    <div class="blob">
-
-    </div>
-
-<!-- Blob -->
+        <button id="loginButton"> Login </button>
 
 
-    <!-- JavaScript -->
-    <script src="assets/js/main.js"></script>
-<?php
-    include("_footer.php");
-?>
+        <div id="loginModal" class="modal">
+            <div class="modal-content">
+                <span class="close" id="closeModal"> <i class="bi-x"></i></span>
+                <h2 style="text-align: center;margin-top: -20px;" >Login</h2><br>
+                <form method="post" id="login">
+                        <legend style="margin-top: -20px;">Guess a Username and Password !</legend>
+                        <p>
+                            <label for="user"><i class="bi-person"></i></label>
+                            <input type="text" placeholder="Username" value="<?php echo $pseudo ? $pseudo : ""; ?>" name="pseudo" id="user" size="20" maxlength="10" required autofocus />
+                        </p>
+                        <p>
+                            <label for="psw"><i class="bi-key"></i> </label>
+                            <input type="password" placeholder="*******" value="<?php echo $pseudo ? $password : ""; ?>" name="password" id="psw" size="20" required />
+                        </p>
+                        <input type="submit" value="Test credential !" id="button" style="margin-top: 30px;">
+
+                </form>
+            </div>
+        </div>
+
+
+    <!-- Blob  -->
+        <div class="blob">
+
+        </div>
+
+    <!-- Blob -->
+
+
+        <!-- JavaScript -->
+        <script src="assets/js/main.js"></script>
+		<?php include("_footer.php"); ?>
+</body>
+</html>
